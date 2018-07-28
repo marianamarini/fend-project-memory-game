0570dcde505af5cda47d363e6e7c17a952590d1f
Mariana Marini <marini.mariana@gmail.com>
2018-07-28 18:02:56 -0500

    added list of symbols for cards in JS

--- js/app.js
+++ js/app.js
@@ -2,6 +2,12 @@
  * https://www.youtube.com/watch?v=G8J13lmApkQ
  * Create a list that holds all of your cards
  */
+ 
+ const symbols = [ "fa fa-paper-plane-o", "fa fa-paper-plane-o", "fa fa-anchor" , "fa fa-anchor" ,
+ "fa fa-diamond" , "fa fa-diamond" , "fa fa-bolt" ,"fa fa-bolt" ,"fa fa-cube" ,"fa fa-cube" ,
+ "fa fa-leaf" , "fa fa-leaf" , "fa fa-bicycle" , "fa fa-bicycle" , "fa fa-bomb" , "fa fa-bomb" ];
+ 
+ 
 
 
 /*
